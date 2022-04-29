5010dba57be7bec664d35f5c09f272e08f6402ec
Hamidkn <hamid.keshmiri@gmail.com>
2022-04-30 00:59:21 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Application for reading a XML file
+
+There is an XML file that includes products information.
+The goal is to implement three tasks for 
+1) the number of products
+2) the name of the products
+3) the spare part of each product which may some product does not have any parts.
+   
+Each task is implemented using buttons that recover required information related to each task.
+
+The app is implemented using node.js and express.js. For reading an XML file the xml2js is used.
+
+## Deploy:
+The application is deployed in the following link: 
+```
+```
+## Usage: 
+
+First, install dependencies in package.json by the following command:
+
+``` npm install ```
+
+Then run the application.
+
+``` node ./app.js ```
+
+because the XML file is too large, it takes more seconds to show the information. So, be patient :).
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const xml2js = require('xml2js');
+const fs = require('fs');
+const { DOMParser } = require('xmldom');
+const { parse } = require('path');
+const util = require('util');
+
+const parser = new xml2js.Parser({
+    explicitArray: false,
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.set("view engine", 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+let xmlData = fs.readFileSync('export_full.xml', 'utf8');
+
+function parseXml(xmlData, res, name) {
+    parser.parseString(xmlData, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render(name, { Items: result.export_full.items.item });
+        }
+    });
+};
+
+app.route('/')
+    .get((req, res) => {
+        res.render('index', { Items: null });
+    }).post((req, res) => {
+        parseXml(xmlData, res, 'index');
+    });
+
+app.route('/names')
+    .get((req, res) => {
+        res.render('names', { Items: null });
+    }).post((req, res) => {
+        parseXml(xmlData, res, 'names');
+    });
+
+app.route('/parts')
+    .get((req, res) => {
+        res.render('parts', { Items: null });
+    }).post((req, res) => {
+        parseXml(xmlData, res, 'parts');
+    });
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
